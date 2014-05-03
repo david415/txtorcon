7e8a376a79f01069d537e9cd4403e2ca23d70345
David Stainton <dstainton415@gmail.com>
2014-05-03 22:57:18 +0000

    Fix endpoint parser
    The endpoint parser now returns an endpoint object like it should...
    all the launch tor and wait logic should be placed in the endpoint's
    listen method.

--- examples/hiddenservice_endpoint_parser.py
+++ examples/hiddenservice_endpoint_parser.py
@@ -15,31 +15,12 @@ import txtorcon
 class TorHiddenServiceEndpointStringParser(object):
     prefix = "onion"
 
-    def setup_hidden_service(self, tor_process_protocol):
-        config      = txtorcon.TorConfig(tor_process_protocol.tor_protocol)
-        hs_endpoint = txtorcon.TCPHiddenServiceEndpoint(reactor, config, self.publicPort)
-        return hs_endpoint
+    def _parseServer(self, reactor, socksPort=None, controlPort=None, publicPort=None, dataDir=None):
 
-    def updates(self, prog, tag, summary):
-        print "%d%%: %s" % (prog, summary)
-
-    def _parseServer(self, reactor, socksPort=None, controlPort=None, publicPort=None):
         assert publicPort is not None
-        assert (socksPort and controlPort) is not None
 
-        self.publicPort    = int(publicPort)
-
-        config             = txtorcon.TorConfig()
-        config.socksPort   = int(socksPort)
-        config.ControlPort = int(controlPort)
-
-        # optional progress output
-        #progress_updates   = self.updates
-        progress_updates   = None
-
-        d = txtorcon.launch_tor(config, reactor, progress_updates=progress_updates, timeout=60)
-        d.addCallback(self.setup_hidden_service)
-        return d
+        hs_endpoint = txtorcon.TCPHiddenServiceEndpoint(reactor, public_port=publicPort, data_dir=dataDir, socks_port=socksPort, control_port=controlPort)
+        return hs_endpoint
 
     def parseStreamServer(self, reactor, *args, **kwargs):
         return self._parseServer(reactor, *args, **kwargs)
